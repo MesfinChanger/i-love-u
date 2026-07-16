fbc23918c2ce06c027830abd9e668e2ee6475f8d
Mesfin Zergaw <zergawmesfin2@gmail.com>
2026-07-16 21:52:52 +0000

    src/constants/categories.ts export const shoppingCategories=[
    "Fashion"

---
+++ src/constants/categories.ts
@@ -0,0 +1,17 @@
+/**
+ * @fileOverview Standardized Shopping Categories for the Prosperity Marketplace.
+ * Used to classify artisan stores and product listings globally.
+ */
+
+export const shoppingCategories = [
+  "Fashion",
+  "Electronics",
+  "Food",
+  "Home",
+  "Books",
+  "Services",
+  "Handmade",
+  "Technology",
+  "Education",
+  "Other"
+];
